302f4c782566ab2bedf9349cd63aaa40599e721b
AbrianaJ <abjanelle@outlook.com>
2025-01-12 17:58:04 -0500

    added secrets folder

--- gpt-wrapper.py
+++ gpt-wrapper.py
@@ -1,3 +1,15 @@
 #This file will serve as out GPT connection 
 
-#change test
+
+from openai import OpenAI
+import streamlit as st
+
+api_key = st.secrets["api_key"]
+client = OpenAI(api_key = api_key)
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "user", "content": "write a haiku about ai"}
+    ]
+)
+print(completion.choices[0].message.content)
